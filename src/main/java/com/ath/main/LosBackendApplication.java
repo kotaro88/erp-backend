ad507531864fdd3ed564a8b6cf378a19e992d992
kotaro88 <ducanhttm@gmail.com>
2018-07-05 08:53:55 +0700

    the first

---
+++ src/main/java/com/ath/main/LosBackendApplication.java
@@ -0,0 +1,12 @@
+package com.ath.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LosBackendApplication.class, args);
+	}
+}
